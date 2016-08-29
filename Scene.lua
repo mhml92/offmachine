eb7cc6af32ee2126ad7c679b09adb23aaa12f9d7
Mikkel Levisen <mikkel.levisen@gmail.com>
2016-08-29 18:55:16 +0200

    refactoring

--- Scene.lua
+++ Scene.lua
@@ -13,7 +13,7 @@ function Scene:initialize()
 	self.layers = {}
 	self.layers.default = 0
 	self.layerId = 0
-   self.collision = Collision:new(self)
+	self.collision = Collision:new(self)
 end
 
 function Scene:update(dt)
@@ -35,99 +35,99 @@ end
 function Scene:draw()
 
 	table.sort(self.entities,
-	function(a,b) 
-		if a.layer < b.layer then 
-			return true 
-		elseif a.layer == b.layer then 
-			if a.id < b.id then 
+		function(a,b) 
+			if a.layer < b.layer then 
 				return true 
+			elseif a.layer == b.layer then 
+				if a.id < b.id then 
+					return true 
+				else 
+					return false 
+				end 
 			else 
 				return false 
 			end 
-		else 
-			return false 
-		end 
-	end)
+		end)
 
-	for i, v in ipairs(self.entities) do
-		if v:isActive() then
-			v:draw()
+		for i, v in ipairs(self.entities) do
+			if v:isActive() then
+				v:draw()
+			end
 		end
 	end
-end
 
-function Scene:addEntity(e,layer)
-	if not layer then
-		layer = self.layers.default
+	function Scene:addEntity(e,layer)
+		if not layer then
+			layer = self.layers.default
+		end
+
+		e:setId(self:getNewId())
+		e:setLayer(layer)	
+		table.insert(self.entities, e)
+		return e
 	end
 
-	e:setId(self:getNewId())
-	e:setLayer(layer)	
-	table.insert(self.entities, e)
-	return e
-end
+	function Scene:resetInput()
+		for key,v in pairs(self.keyDown) do
+			if self.keyUp[key] then
+				self.keyDown[key] = nil
+				self.keyUp[key] = nil
+			end
+		end
 
-function Scene:resetInput()
-   for key,v in pairs(self.keyDown) do
-      if self.keyUp[key] then
-         self.keyDown[key] = nil
-         self.keyUp[key] = nil
-      end
-   end
+		for key,v in pairs(self.mouseDown) do
+			if self.mouseUp[key] then
+				self.mouseDown[key] = nil
+				self.mouseUp[key] = nil
+			end
+		end
+	end
 
-   for key,v in pairs(self.mouseDown) do
-      if self.mouseUp[key] then
-         self.mouseDown[key] = nil
-         self.mouseUp[key] = nil
-      end
-   end
-end
+	function Scene:keypressed(key, isrepeat)
+		self.keyDown[key] = true
+	end
 
-function Scene:keypressed(key, isrepeat)
-	self.keyDown[key] = true
-end
+	function Scene:keyreleased(key, isrepeat)
+		self.keyUp[key] = true
+	end
 
-function Scene:keyreleased(key, isrepeat)
-	self.keyUp[key] = true
-end
+	function Scene:mousepressed(x,y,button)
+		self.mouseDown[button] = true
+	end
 
-function Scene:mousepressed(x,y,button)
-	self.mouseDown[button] = true
-end
+	function Scene:mousereleased(x,y,button)
+		self.mouseUp[button] = true
+	end
 
-function Scene:mousereleased(x,y,button)
-	self.mouseUp[button] = true
-end
+	function Scene:addLayer(name)
+		self.layers[name] = self:getNewLayerId()
+	end
 
-function Scene:addLayer(name)
-	self.layers[name] = self:getNewLayerId()
-end
+	function Scene:getNewLayerId()
+		local nid = self.layerId
+		self.layerId = self.layerId + 1
+		return nid
+	end
 
-function Scene:getNewLayerId()
-	local nid = self.layerId
-	self.layerId = self.layerId + 1
-	return nid
-end
-
-function Scene:getNewId()
-	local nid = self.entitiesId
-	self.entitiesId = self.entitiesId + 1
-	return nid
-end
+	function Scene:getNewId()
+		local nid = self.entitiesId
+		self.entitiesId = self.entitiesId + 1
+		return nid
+	end
 
 
-function Scene:beginContact(a,b,coll)
-   self.collision:resolve(a,b,coll)
-end
+	function Scene:beginContact(a,b,coll)
+		self.collision:resolve(a,b,coll)
+	end
 
-function Scene:endContact(a,b,coll)
-end
+	function Scene:endContact(a,b,coll)
+	end
 
 
-function Scene:preSolve(a,b,coll)
-end
+	function Scene:preSolve(a,b,coll)
+	end
 
-function Scene:postSolve(a, b, coll, normalimpulse1, tangentimpulse1, normalimpulse2, tangentimpulse2)
-end
+	function Scene:postSolve(a, b, coll, normalimpulse1, tangentimpulse1, normalimpulse2, tangentimpulse2)
+	end
 
-return Scene
+	return Scene
